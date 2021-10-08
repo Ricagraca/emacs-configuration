d2e827865e57696dcbe6b61f3df82a177ad4b017
ricagraca <ricagraca@hotmail.com>
2021-10-08 13:35:17 +0200

    Added a rust-mode package to my emacs configuration file

--- init.el
+++ init.el
@@ -1,4 +1,24 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 (package-refresh-contents)
+
+
+(dolist (package '(rust-mode))
+ (unless (package-installed-p package)
+   (package-install package))
+   (require package))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(rust-mode use-package proof-general multiple-cursors elpy company-coq)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
